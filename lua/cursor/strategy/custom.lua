d8b9b18e541203bea169ff3174d91f969a224703
rasulomaroff <rasulomaroff@gmail.com>
2023-07-16 00:41:36 +0300

    feat(strategy): adds custom strategy

---
+++ lua/cursor/strategy/custom.lua
@@ -0,0 +1,35 @@
+local Util = require 'cursor.util'
+
+local M = {
+    _initialized = false,
+    _triggered = false,
+    _cursor = nil,
+}
+
+-- noop while not initialized
+function M.trigger() end
+function M.clear() end
+
+--- @param cursor string
+function M:init(cursor)
+    self._initialized = true
+    self._cursor = cursor
+
+    function M.trigger()
+        if M._triggered then
+            return
+        end
+
+        Util.cursor.set(cursor)
+    end
+
+    function M.clear()
+        if not M._triggered then
+            return
+        end
+
+        Util.cursor.del(cursor)
+    end
+end
+
+return M
