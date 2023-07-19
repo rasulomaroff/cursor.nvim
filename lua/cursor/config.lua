a1940922f5afa1b164a764c85325f8170186163a
rasulomaroff <rasulomaroff@gmail.com>
2023-07-19 22:07:00 +0300

    refactor(config)!: changes defaults and moves config initialization in config file

--- lua/cursor/config.lua
+++ lua/cursor/config.lua
@@ -16,21 +16,54 @@
 --- @field events? string | string[] -- additional events to trigger a cursor blinking
 --- @field overwrite_events? boolean -- overwrite default events
 
---- @type Cursor.Config
-return {
+local default_config = {
     trigger = {
-        strategy = {
-            type = 'event',
-            timer = {
-                delay = 5000,
-                events = nil,
-                overwrite_events = false,
-            },
-        },
-        cursors = {
-            { mode = 'a', blink = { wait = 100, freq = 400 } },
-        },
+        strategy = nil,
+        cursors = nil,
     },
     overwrite_cursor = false,
     cursors = nil,
 }
+
+local M = {}
+
+---@param config? Cursor.Config
+function M.setup(config)
+    config = config and vim.tbl_deep_extend('force', default_config, config) or default_config
+
+    vim.validate {
+        trigger = { config.trigger, 't' },
+        cursors = { config.cursors, 't', true },
+        overwrite_cursor = { config.overwrite_cursor, 'b', true },
+    }
+
+    local Cursor = require 'cursor.cursor'
+    local Util = require 'cursor.util'
+
+    if config.overwrite_cursor then
+        vim.opt.guicursor = ''
+    end
+
+    if config.cursors and not vim.tbl_isempty(config.cursors) then
+        Cursor.set(Cursor.extract_list(config.cursors))
+    end
+
+    local strategy_type
+    if config.trigger.strategy then
+        strategy_type = config.trigger.strategy.type
+    end
+
+    if not Util.has_strategy(strategy_type) then
+        return
+    end
+
+    vim.validate {
+        ['trigger.cursors'] = { config.trigger.cursors, 't' },
+    }
+
+    Cursor.extract_list(config.trigger.cursors, true)
+
+    require('cursor.strategy.' .. strategy_type):init(config.trigger.strategy[strategy_type])
+end
+
+return M
--- lua/cursor/init.lua
+++ lua/cursor/init.lua
@@ -1,37 +1,7 @@
-local Util = require 'cursor.util'
-local Cursor = require 'cursor.cursor'
-
 local M = {}
 
---- @param config? Cursor.Config
-function M.setup(config)
-    -- TODO: reload it if setup is called more than once
-
-    -- TODO: validating?
-    -- vim.validate {}
-
-    local default_config = require 'cursor.config'
-
-    config = config and vim.tbl_deep_extend('force', default_config, config) or default_config
-
-    if config.overwrite_cursor then
-        vim.opt.guicursor = ''
-    end
-
-    if config.cursors and not vim.tbl_isempty(config.cursors) then
-        Cursor.set(Cursor.extract_list(config.cursors))
-    end
-
-    local strategy_type = config.trigger.strategy.type
-
-    if not Util.is_strategy(strategy_type) then
-        return
-    end
-
-    local triggered_cursor = Cursor.extract_list(config.trigger.cursors)
-
-    require('cursor.strategy.' .. strategy_type):init(triggered_cursor, config.trigger.strategy[strategy_type])
-end
+-- TODO: reload it if setup is called more than once
+M.setup = require('cursor.config').setup
 
 -- TODO: support reloading
 function M.deactivate()
